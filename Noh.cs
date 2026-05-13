f91622f04b3391ec51d6d17ea359afca8dd174f2
Kauan Koenigkan <281706847+kauankoenigkan@users.noreply.github.com>
2026-05-13 16:17:50 -0300

    Add files via upload

---
+++ Noh.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace PilhaDinamicaESTUDOS
+{
+    internal class Noh
+    {
+        string valor;
+        Noh anterior;
+
+        public string Valor
+        {
+            get { return valor; }
+            set { valor = value; }
+        }
+
+        public Noh Anterior
+        {
+            get { return anterior; }
+            set { anterior = value; }
+        }
+    }
+}
