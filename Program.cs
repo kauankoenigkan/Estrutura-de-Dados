7fc388f5d57af0de47b5995d5588cb56fdad6067
Kauan Koenigkan <281706847+kauankoenigkan@users.noreply.github.com>
2026-05-13 16:01:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using PilhaDinamicaESTUDOS;
+
+Pilha agenda = new Pilha();
+agenda.Empilhar("A");
+agenda.Empilhar("B");
+agenda.Empilhar("C");
+agenda.Empilhar("D");
+agenda.Empilhar("E");
+
+Console.WriteLine("PRIMEIRA IMPRESSÃO");
+agenda.Imprimir();
+Console.WriteLine();
+
+agenda.Desempilhar();
+Console.WriteLine("IMPRESSÃO após Desenfileirar");
+agenda.Imprimir();
